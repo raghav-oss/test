2bbb7a531cd5bed03bdd32a28727a28ecb3a1fe3
raghav-oss <154777980+raghav-oss@users.noreply.github.com>
2024-03-02 14:17:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const sql = require('mssql');
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());

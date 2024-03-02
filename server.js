480c9a6df90aa8540d63fb64c4554c17b51d9363
raghav-oss <154777980+raghav-oss@users.noreply.github.com>
2024-03-02 10:26:33 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const sql = require('mssql');
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const config = {
+    user: 'sa',
+    password: 'duke1234',
+    server: 'SAP-3-156',
+    database: 'Project_db',
+    options: {
+        encrypt: false,
+        trustServerCertificate: true,
+    },
+};
+app.get('/customers', async (req, res) => {
+    try {
+        // Connect to the database
+        const pool = await sql.connect(config);
+        // Query the database to fetch all rows from CustMaster table
+        const result = await pool.request().query('SELECT * FROM CustMaster');
+        // Send the fetched data as JSON response
+        res.json({ success: true, customers: result.recordset });
+    } catch (error) {
+        // Handle errors
+        console.error('Error:', error.message);
+        res.status(500).json({ success: false, error: error.message });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
